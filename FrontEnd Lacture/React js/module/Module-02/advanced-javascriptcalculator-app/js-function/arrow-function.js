956eae184506720cc9c512a0804ac49de23745fc
Rakesh Dangar <142068632+Rakeshdangarr@users.noreply.github.com>
2024-06-25 18:58:35 +0530

    update

---
+++ FrontEnd Lacture/React js/module/Module-02/advanced-javascriptcalculator-app/js-function/arrow-function.js
@@ -0,0 +1,11 @@
+// Arrow functions were introduced in ES6.
+
+// Arrow functions allow us to write shorter function syntax:
+
+const ad=()=>{
+    var a = 10;
+    var b = 20;
+    var c = a+b;
+    console.log(c)
+}
+ad()
