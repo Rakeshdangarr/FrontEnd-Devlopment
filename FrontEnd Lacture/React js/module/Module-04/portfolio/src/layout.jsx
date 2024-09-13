db3a63cd31d3f1802b96516eb7d8161adeb642c8
Rakesh Dangar <142068632+Rakeshdangarr@users.noreply.github.com>
2024-09-13 12:19:39 +0530

    update

---
+++ FrontEnd Lacture/React js/module/Module-04/portfolio/src/layout.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./components/Header";
+import Slider from "./components/Slider";
+import Content from "./components/Content";
+
+
+
+
+
+
+const Layout=()=>{
+
+    return(
+
+        <>
+        {/* header here */}
+        <Header/>
+
+        {/* Slider here */}
+        <Slider/>
+
+        {/* Content here */}
+        <Content/>
+        </>
+    )
+}
+export default Layout
