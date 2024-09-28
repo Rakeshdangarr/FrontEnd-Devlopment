cd0f925b0666680e193fde2db28064a800f6f6b3
Rakesh Dangar <142068632+Rakeshdangarr@users.noreply.github.com>
2024-09-28 14:59:38 +0530

    update

---
+++ FrontEnd Lacture/React js/module/Module-06/react-components/class-components-app/src/components/Employee.jsx
@@ -0,0 +1,16 @@
+
+import React,{Component} from "react";
+class Employee extends Component
+{
+    render()
+    {
+        return(
+            <>
+                <h1>The employee name is :Brijesh</h1>
+            </>
+        )
+    }
+
+}
+
+export default Employee
