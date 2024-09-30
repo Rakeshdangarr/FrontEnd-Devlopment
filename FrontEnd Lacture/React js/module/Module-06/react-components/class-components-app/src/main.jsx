29567ec911392a89ed60a0d86580e2a696e9ed9c
Rakesh Dangar <142068632+Rakeshdangarr@users.noreply.github.com>
2024-09-30 12:39:35 +0530

    update

--- FrontEnd Lacture/React js/module/Module-06/react-components/class-components-app/src/main.jsx
+++ FrontEnd Lacture/React js/module/Module-06/react-components/class-components-app/src/main.jsx
@@ -1,10 +1,13 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+// import Employee from './components/Employee'
 import './index.css'
+import EmployeeData from './components/EmployeeData'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <Employee /> */}
+    <EmployeeData />
   </StrictMode>,
 )
