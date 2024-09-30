641bd679b629f1cbc3d5c87f8a003cac67a6d8c7
Rakesh Dangar <142068632+Rakeshdangarr@users.noreply.github.com>
2024-09-30 12:52:56 +0530

    update

---
+++ FrontEnd Lacture/React js/module/Module-06/react-components/function-components-app/src/components/car.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+function Car()
+{
+    return(
+        <>
+            <h1>THis is a mercedes Car</h1>
+        </>
+    )
+}
+function Garage()
+{
+    return(
+        <>
+            <Car />
+            <h1>Mercedes is living inside of Mr. Pintu patel Garage</h1>
+        </>
+    )
+}
+
+export default Garage
--- FrontEnd Lacture/React js/module/Module-06/react-components/function-components-app/src/main.jsx
+++ FrontEnd Lacture/React js/module/Module-06/react-components/function-components-app/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Garage from './components/Car'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Garage />
   </StrictMode>,
 )
