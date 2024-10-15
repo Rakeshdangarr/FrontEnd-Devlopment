55668305a6bad1266056f21ccff56faff8e85af0
Rakesh Dangar <142068632+Rakeshdangarr@users.noreply.github.com>
2024-10-15 10:21:40 +0530

    Delete Module (Assignment)/Assignment-10 (React Js)/ReactJs module-01/todo-app directory

--- Module (Assignment)/Assignment-10 (React Js)/ReactJs module-01/todo-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import GroceryApp from './components/groceryApp'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <GroceryApp/>
-  </StrictMode>,
-)
